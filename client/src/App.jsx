ea9bd40262ca91199d0dff72da0da095268835fb
shahreaz <ashahreaz@gmail.com>
2021-09-22 03:45:28 +0600

    Add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 // components
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -9,6 +10,7 @@ import Register from "./components/Register";
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button } from "antd";
+import "./Navbar.css";
+
+const Navbar = () => {
+	return (
+		<div className="layout">
+			<nav className="navbar">
+				<label
+					className="navbar-toggle"
+					id="js-navbar-toggle"
+					htmlFor="chkToggle"
+				>
+					<i class="bx bx-menu"></i>
+				</label>
+				<NavLink to="/" className="logo">
+					acpn
+				</NavLink>
+				<input type="checkbox" id="chkToggle"></input>
+				<ul className="main-nav" id="js-menu">
+					<li className="nav-li">
+						<NavLink to="/login" className="nav-links">
+							<Button type="primary">Login</Button>
+						</NavLink>
+					</li>
+					<li className="nav-li">
+						<NavLink to="/register" className="nav-links">
+							<Button>Register</Button>
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
+
+export default Navbar;
