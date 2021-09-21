4adad4a909ef315732533e6517fcb5be870708e3
shahreaz <ashahreaz@gmail.com>
2021-09-22 00:47:49 +0600

    Setup react router with proper components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+// components
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Frontend theke bolchi.</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
     </div>
   );
 };
